bb8f118b67dc8f401dd7937b3604496c76e6059d
iratusbison <130743703+iratusbison@users.noreply.github.com>
2023-10-09 10:55:29 +0530

    Create view_expense.py

---
+++ itemmanager/views/view_expense.py
@@ -0,0 +1,28 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from itemmanager.models.expense import Expense
+from itemmanager.forms import ExpenseForm
+from django.db.models import Sum
+
+def expense_list(request):
+    expenses = Expense.objects.all()
+
+    total_expenses = expenses.aggregate(total=Sum('amount'))['total']
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses, 'total_expenses': total_expenses})
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', {'form': form})
+
+def delete_expense(request, expense_id):
+    expense = get_object_or_404(Expense, pk=expense_id)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('expense_list')
+    return render(request, 'expenses/delete_expense.html', {'expense': expense})
